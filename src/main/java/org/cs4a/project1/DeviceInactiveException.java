50cdbfdc729b0669795e54edca69714bb97166ae
aidannguyen966 <aidannguyen966@gmail.com>
2026-03-20 11:12:31 -0700

    Add files via upload

---
+++ src/main/java/org/cs4a/project1/DeviceInactiveException.java
@@ -0,0 +1,15 @@
+package org.cs4a.project1;
+
+public class DeviceInactiveException extends Exception {
+
+    private final String deviceName;
+
+    public DeviceInactiveException(String deviceName) {
+        super("Cannot perform operation on \"" + deviceName + "\": device is not active. Turn the device on first.");
+        this.deviceName = deviceName;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+}
---
+++ src/main/java/org/cs4a/project1/InvalidSpeedException.java
@@ -0,0 +1,24 @@
+package org.cs4a.project1;
+
+public class InvalidSpeedException extends Exception {
+
+    private final int invalidSpeed;
+
+    public InvalidSpeedException(int speed) {
+        super(buildMessage(speed));
+        this.invalidSpeed = speed;
+    }
+
+    public int getInvalidSpeed() {
+        return invalidSpeed;
+    }
+
+    private static String buildMessage(int speed) {
+        if (speed < 0) {
+            return "Invalid speed: " + speed + ". Speed cannot be negative.";
+        } else {
+            return "Invalid speed: " + speed + ". Speed cannot exceed 100%.";
+        }
+    }
+}
+ 
